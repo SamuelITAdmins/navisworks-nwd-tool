0848d101f103b35b8c22549dae0d54eebb435832
SPozder3 <scottpozder@gmail.com>
2025-02-03 22:58:40 -0500

    Updated conversion powershell script so that it stalls until the conversion process is complete

--- scripts/convertNWFfile.ps1
+++ scripts/convertNWFfile.ps1
@@ -21,4 +21,13 @@ $arguments = "-OpenFile " + $nwfPath + " -NoGui -SaveFile " + $nwdPath + " -Exit
 # convert to nwd and save
 Start-Process -filepath $navisworksPath -Argumentlist $arguments
 
+# Wait until the temporary .nwd~ file disappears
+$tempFile = $nwdPath + "~"
+Write-Host "Waiting for temporary file to be removed: $tempFile"
+
+# Stall until the temporary file no longer exists
+while (Test-Path $tempFile) {
+    Start-Sleep -Seconds 1
+}
+
 Write-Host "Success: NWD generated $nwdPath"
